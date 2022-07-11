dad4277312a9d37120d2aa51f0eaca7d395711d7
ItzMari0 <msajr@outlook.com>
2022-07-11 18:29:06 -0300

    Requisito 1 resolvido

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (product) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  if (url.endsWith('undefined')) {
+    return new Error('You must provide an url');
+  }
+  const list = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
+
+  return list;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -11,4 +11,19 @@ describe('1 - Teste a função fetchProducts', () => {
     fetchProducts('computador');
     expect(fetch).toHaveBeenCalledTimes(1);
   });
+
+  it('se ao chamar a função "fetchProducts" com o argumento computador, a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', () => {
+    fetchProducts('computador');
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+
+  it('se o retorno da função "fetchProducts" com o argumento "computador", é uma estrutura de dados igual ao objeto "computadorSearch"', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+
+  it('ao chamar a função "fetchProducts" sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => {
+    const failRequest = await fetchProducts();
+    expect(failRequest).toEqual(new Error('You must provide an url'));
+  });
 });

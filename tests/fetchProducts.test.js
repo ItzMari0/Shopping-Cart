5a7c3048f23455a4cdd81a78bc7dc0df790ca0a4
ItzMari0 <msajr@outlook.com>
2022-07-11 15:22:24 -0300

    Iniciando o Projeto!

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,12 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Deve ser uma função', () => {
+    expect(typeof(fetchProducts)).toEqual('function');
+  });
+
+  it('chama a fetch quando a função "fetchProducts" é chamada com o argumento "computador"', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
 });

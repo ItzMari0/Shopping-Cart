2e03b1fd8c55475da376d6e8e9b4255ee586b854
ItzMari0 <msajr@outlook.com>
2022-07-11 21:32:20 -0300

    requisito 2 resolvido

--- script.js
+++ script.js
@@ -15,12 +15,10 @@ const createCustomElement = (element, className, innerText) => {
 const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 };
 
@@ -38,4 +36,13 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-window.onload = () => { };
+const filterProducts = async () => {
+  const { results } = await fetchProducts('computador');
+  const items = document.querySelector('.items');
+  results.forEach(({ id: sku, title: name, thumbnail: image }) => {
+    const product = createProductItemElement({ sku, name, image });
+    items.appendChild(product);
+  });
+};
+
+window.onload = () => { filterProducts(); };
